89ff5475d9a416e21ca7b86a7b9aacd529104a53
Andrei Tenescu <Andrei Tenescu>
2016-05-22 05:17:05 +0300

    More fixes to the algorithm

--- CarRecon3/main.py
+++ CarRecon3/main.py
@@ -6,7 +6,7 @@ car_cascade = [cv2.CascadeClassifier('test.xml')]
 colors = [(0,255,0)]
 
 #open video
-cap = cv2.VideoCapture('inter3.avi')
+cap = cv2.VideoCapture('inter9.avi')
 
 #dimensions of video
 wid = 1280
@@ -16,15 +16,16 @@ scale = 2 #we divide by this
 
 #defines our intersection
 #points = np.array([(50,125),(900,60),(1280,150),(1280,260),(244,450)])
-points = np.array([(50,175),(500,120),(900,150),(900,290),(180,480)])
-entries = [(50,175),(410,130),(650,130),(820,145),(900,290),(900,190), (50,175),(180,480)]
-exits = [(410,130),(500,120),(500,120),(650,130),(820,145),(900,150),(900,150),(900,190),(900,290),(180,480)]
+points = np.array([(50,155),(500,110),(900,150),(900,290),(180,480)])
+entries = [(50,155),(410,115),(630,120),(820,145),(900,290),(900,190), (75,230),(180,480)]
+exits = [(410,115),(500,110),(500,110),(640,120),(820,145),(900,150),(900,150),(900,190),(900,290),(180,480),(75,230),(50,155)]
 
 id = 0
 
 v_points = {}
 v_points_entry = {}
 v_points_exit = {}
+v_points_check = {}
 
 def distToLine(line_s, line_e, point):
     normalLength = np.hypot(line_e[0] - line_s[0], line_e[1] - line_s[1])
@@ -60,10 +61,14 @@ def checkY(origY):
 def distance((a1,b1),(a2,b2)):
     return np.sqrt(np.power(a1-a2,2)+np.power(b1-b2,2))
 
+f_count = 0
+
 #main loop
 while cap.isOpened():
     #image aquisition and cascade detection
     ret, orig_1 = cap.read()
+
+    f_count += 1
 
     if not ret:
         break
@@ -75,6 +80,12 @@ while cap.isOpened():
     cars = car_cascade[0].detectMultiScale(gray, 1.1, 1)
 
     current_points = list()
+
+    cv2.putText(frame, str(f_count), (0,100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (255, 255, 255), 2)
+
+    # if f_count > 180:
+    #     if cv2.waitKey(10000) & 0xFF == ord('q'):
+    #         break
 
     #detect cascades and display them
     for i in range(0, len(points)):
@@ -96,7 +107,7 @@ while cap.isOpened():
     for i in range(0,4):
         cv2.line(frame,entries[i*2],entries[i*2+1], (0,0,0),3)
 
-    for i in range(0, 5):
+    for i in range(0, 6):
         cv2.line(frame, exits[i * 2], exits[i * 2 + 1], (0, 0, 255), 3)
 
     #go through list of identified cascades
@@ -106,16 +117,22 @@ while cap.isOpened():
         min_id = 0
         min_point = (x,y)
 
-        for i,(m,n) in v_points.iteritems():
+        #cv2.circle(frame, (x, y), 10, (122, 122, 122),5)
+        #cv2.putText(frame, str(x)+";"+str(y), (x, y+15), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (255, 255,255))
+
+        for i, (m, n) in v_points.iteritems():
             dist = distance((x,y),(m,n))
+
+            #print "Distance from x,y:"+str(x)+";"+str(y)+" circle to id "+str(i) +": "+str(dist)+" at " + str(m)+";"+str(n)
+
+            #cv2.line(frame, (x,y), (m,n), (122, 122, 0), 3)
             if dist < 300/scale and dist < min:
                 min = dist
                 found = True
                 min_id = i
                 min_point = (x,y)
 
-
-        if(cv2.pointPolygonTest(points, (x,y), True)) >= 15/scale:
+        if cv2.pointPolygonTest(points, (x,y), True) >= 15:
             if found:
                 v_points[min_id] = min_point
             else:
@@ -123,6 +140,8 @@ while cap.isOpened():
                 if d > 50:
                     v_points[id] = min_point
                     v_points_entry[id] = min_point
+                    temp = (min_point, f_count)
+                    v_points_check[id] = temp
                     id = id +1
 
     to_be_deleted = list()
@@ -131,27 +150,53 @@ while cap.isOpened():
         cv2.circle(frame, (x,y), 25, (255,255,255))
         cv2.putText(frame, str(i), (x,y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (255, 255, 255), 2)
 
+        #print "Id " + str(i) + "is at "+str(x)+";"+str(y)
+        d2 = 3000
         if i in v_points_entry:
             d = distToLineArr(exits,(x,y))
-            if d < 25:
+            d2 = distance(v_points_entry[i],(x,y))
+
+            #print "Distance to exit:" + str(d)+ "; distance to entry: "+str(d2)
+
+            if d < 30 and d2 > 70:
                 v_points_exit[i] = (x, y)
                 to_be_deleted.append(i)
 
+        #v_points_check will remember the position of a point so that it can be removed if it stays stationary for too long
+        var = v_points_check[i]
+        if var[0] != (x,y):
+            v_points_check[i] = ((x,y),f_count)
+        else:
+            cv2.circle(frame, (x, y), 25, (0, 0, 255),2)
+            dte = distToLineArr(exits,(x,y))
+            if dte<60 and d2 > 70:
+                v_points_exit[i] = (x, y)
+                to_be_deleted.append(i)
+            else:
+                print "Id "+str(i)+"is this far away from exit: "+str(dte)
+
+
+        if f_count - v_points_check[i][1] > 10:
+            to_be_deleted.append(i)
+
     for i in to_be_deleted:
         v_points.pop(i,None)
+        v_points_check.pop(i,None)
 
     for i,(x,y) in v_points_entry.iteritems():
-        cv2.circle(frame, (x, y), 25, (0, 255, 0))
-        cv2.putText(frame, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.circle(frame, (x, y), 5, (0, 255, 0))
+        cv2.putText(frame, str(i), (x+5, y+5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.75, (0, 255, 0))
 
     for i, (x, y) in v_points_exit.iteritems():
-        cv2.circle(frame, (x, y), 25, (0, 0, 255))
-        cv2.putText(frame, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.circle(frame, (x, y), 5, (0, 0, 255))
+        cv2.putText(frame, str(i), (x+5, y+5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.75, (0, 0, 255))
 
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cv2.waitKey()
 
 # When everything done, release the capture
 cap.release()
